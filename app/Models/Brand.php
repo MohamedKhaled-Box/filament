54fd408346d32910927de363c6ba50822ccde368
mohamed khaled <mohamedkhaledx12@gmail.com>
2024-01-07 23:42:58 +0200

    Initial commit

---
+++ app/Models/Brand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Brand extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name', 'slug', 'url', 'primary_hex', 'is_visable', 'description'
+    ];
+    public function brands(): HasMany
+    {
+        return $this->hasmany(Product::class);
+    }
+}
