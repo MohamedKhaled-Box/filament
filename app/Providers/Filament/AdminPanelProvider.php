b8141f7cfaea587a0b384dab12ffc6feb151e984
mohamed khaled <mohamedkhaledx12@gmail.com>
2024-06-08 16:41:01 +0300

    8-6-2024
    remove the widget for filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+
             ->default()
             ->id('admin')
             ->path('admin')
@@ -44,7 +45,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://skriipta.com/wp-content/uploads/2022/08/skriipta-logo-h-regular.png" alt="skriipta" class="h-10">
+{{-- <img src="https://skriipta.com/wp-content/uploads/2022/08/skriipta-logo-h-regular.png" alt="skriipta" class="h-10"> --}}
